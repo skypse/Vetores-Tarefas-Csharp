5542a0225dce094aab1f372dc29001fc66b112b8
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-16 20:02:13 -0300

    adicionando exercicio 03 e 04

---
+++ Vetores-Tarefas-Csharp-03/vetor-03.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+// Nome     : Gabriel do Amaral Alves
+// Data     : 16/06/2024
+// Enunciado: 3) Média dos Elementos: Desenvolva um programa que calcule
+// a média dos elementos em um vetor de números reais.
+
+namespace Vetores_Tarefas_Csharp_03
+{
+    internal class vetor_03
+    {
+        public static void Main(string[] args)
+        {
+            int[] vetor_inteiros = { 1, 5, 3, 5, 5, 6, 7, 8, 9, 10, 11, 12, };
+
+            double soma = 0;
+            // percorrer o vetor
+            for (int i = 0; i < vetor_inteiros.Length; i++)
+            {
+                soma += vetor_inteiros[i];
+            }
+
+            // media
+            double media = soma / vetor_inteiros.Length;
+
+            Console.WriteLine($"A média dos elementos: {media:F2}");
+        }
+    }
+}
---
+++ Vetores-Tarefas-Csharp-04/vetor-04.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Nome     : Gabriel do Amaral Alves
+// Data     : 16/06/2024
+// Enunciado: 4) Vetor Invertido: Implemente um programa que inverta a ordem dos
+// elementos em um vetor.
+
+namespace Vetores_Tarefas_Csharp_04
+{
+    internal class vetor_04
+    {
+        public static void Main(string[] args)
+        {
+            int[] vetor_inteiros = { 9, 2, 3, 5 };
+
+            int comprimento = vetor_inteiros.Length; // = 4
+
+            for (int i = 0; i < comprimento / 2; i++)
+            {
+                // armazena temporariamente
+                int troca = vetor_inteiros[i]; // troca = 9
+                // substitui o valor do elemento atual pelo valor do último elemento
+                vetor_inteiros[i] = vetor_inteiros[comprimento - 1 - i]; // vetor_inteiros[0] = 5
+                vetor_inteiros[comprimento - 1 - i] = troca; // vetor_inteiros[3] = 9
+            }
+
+            Console.WriteLine("O vetor invertido ficou:");
+            for (int i = 0; i < vetor_inteiros.Length; i++)
+            {
+                Console.Write(vetor_inteiros[i] + " ");
+            }
+        }
+    }
+}
