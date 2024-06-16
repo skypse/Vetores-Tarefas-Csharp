96ba038192f3e748a08ac2f075706979d121dae7
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-16 19:09:57 -0300

    Adicionar arquivos de projeto.

---
+++ Vetores-Tarefas-Csharp-02/vetor-02.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Nome     : Gabriel do Amaral Alves
+// Data     : 16/06/2024
+// Enunciado: 2) Maior e Menor Valor: Crie um programa que encontre
+// o maior e o menor valor em um vetor de números inteiros.
+
+namespace Vetores_Tarefas_Csharp_02
+{
+    internal class vetor_02
+    {
+        public static void Main(string[] args)
+        {
+            int[] vetor_inteiros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, };
+
+            // Máximo
+            Console.WriteLine($"O maior elemento do vetor: {vetor_inteiros.Max()}");
+
+            // Mínimo
+            Console.WriteLine($"O menor elemento do vetor: {vetor_inteiros.Min()}");
+        }
+    }
+}
---
+++ Vetores-Tarefas-Csharp/vetor-01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Nome     : Gabriel do Amaral Alves
+// Data     : 16/06/2024
+// Enunciado: 1) Soma dos Elementos: Escreva um programa que calcule
+// a soma de todos os elementos de um vetor de inteiros.
+
+
+namespace Vetores_Tarefas_Csharp
+{
+    internal class vetor_01
+    {
+        public static void Main(string[] args)
+        {
+            int[] vetor_inteiros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, };
+            Console.WriteLine($"A soma total dos elementos do vetor é: {vetor_inteiros.Sum()}");
+        }
+    }
+}
